5017246b90cc9ce1da403b3de7ea77b64c1b5789
Kerry McAdams <kerry.mcadams@ansys.com>
2023-07-07 15:28:45 -0400

    fixed file format & pre commit config

--- src/ansys/pre_commit_hooks/main.py
+++ src/ansys/pre_commit_hooks/main.py
@@ -1,37 +1,60 @@
-import os
+"""Run reuse for files missing license headers."""
 import datetime
+import os
+import os.path
 import subprocess
 
 from reuse import lint
 from reuse.project import Project
 from reuse.report import ProjectReport
-import os.path
 
 
 def get_files():
-    project = Project('./')
+    """Generate report containing files without the license header."""
+    project = Project("./")
     report = ProjectReport.generate(project)
     return lint.lint_files_without_copyright_and_licensing(report)
-    
-    
+
+
 def run_reuse(file):
+    """Run the reuse command for files missing the license header."""
     year = datetime.date.today().year
-    cmd = ['reuse', 'annotate', '--year', fr'{year}', '--copyright-style', 'string-c', '--merge-copyright', '--template=ansys', '-l', 'MIT', '-c', 'ANSYS, Inc. and/or its affiliates.', '--skip-unrecognised', fr'{file}']
+    cmd = [
+        "reuse",
+        "annotate",
+        "--year",
+        rf"{year}",
+        "--copyright-style",
+        "string-c",
+        "--merge-copyright",
+        "--template=ansys",
+        "-l",
+        "MIT",
+        "-c",
+        "ANSYS, Inc. and/or its affiliates.",
+        "--skip-unrecognised",
+        rf"{file}",
+    ]
     subprocess.check_call(cmd)
 
 
 def main():
+    """Run reuse on files without license headers."""
+    # Get location of main.py
     script_loc = os.path.dirname(__file__)
-    root_dir = fr'{script_loc}/../../../../'
+
+    # Move to pre-commit-hooks directory
+    root_dir = rf"{script_loc}/../../../../"
     os.chdir(root_dir)
-    
-    # Reuse lint  
+
+    # Run reuse lint command to find files without license header
     missing_header = get_files()
-    
+
+    # Run reuse command for files without license header
     for file in missing_header:
         if os.path.isfile(file):
             run_reuse(file)
 
-    
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
