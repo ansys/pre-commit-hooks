6ab227a3bfa9b5b4f45407ac16ef3a05373f96f5
Kerry McAdams <kerry.mcadams@ansys.com>
2023-07-19 16:16:41 -0400

    uncommented __version__

--- tests/__init__.py
+++ tests/__init__.py
@@ -31,4 +31,4 @@ try:
 except ModuleNotFoundError:
     import importlib_metadata
 
-# __version__ = importlib_metadata.version(__name__.replace(".", "-"))
+__version__ = importlib_metadata.version(__name__.replace(".", "-"))
