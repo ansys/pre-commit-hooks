75618ba8af514a45ba6711236d62b250d6fdedf5
Kerry McAdams <kerry.mcadams@ansys.com>
2023-07-19 16:21:31 -0400

    added __version__ to init in src

--- src/ansys/pre_commit_hooks/__init__.py
+++ src/ansys/pre_commit_hooks/__init__.py
@@ -1,1 +1,8 @@
 """Initialize package level imports."""
+
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:
+    import importlib_metadata
+
+__version__ = importlib_metadata.version(__name__.replace(".", "-"))
