d71405e78ccb268520fa1a41a2cd4b2e2895e8e1
Kerry McAdams <kerry.mcadams@ansys.com>
2023-11-14 14:00:04 -0500

    add functionality to remove .reuse and LICENSES dirs

--- tests/test_dependencies.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-def test_template_exists():
-    """Test license format file exists."""
-    ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    jinja_file = os.path.join(ROOT_DIR, ".reuse", "templates", "ansys.jinja2")
-    assert os.path.exists(jinja_file)
