f407591176d47dc82642085a9a773daef61d399c
joshualuoansys <joshua.luo@ansys.com>
2026-02-20 18:16:05 -0500

    fix

--- src/ansys/pre_commit_hooks/add_license_headers.py
+++ src/ansys/pre_commit_hooks/add_license_headers.py
@@ -416,7 +416,8 @@ def set_variables(obj: common.ClickObj, values: dict, args: argparse.Namespace) 
 
     license = [] if args.ignore_license_check else [values["license"]]
     files = values["files"]
-    copyright = [values["copyright"]]
+    #copyright = [values["copyright"]]
+    copyright = values["copyright"].split("\n")
     years = (
         f"{values['start_year']} - {values['current_year']}"
         if values["start_year"] != values["current_year"]
